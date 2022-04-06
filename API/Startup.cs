d66637a83574c9de8a5e9590bec276b78447e6c6
TrickSlayer <90195916+TrickSlayer@users.noreply.github.com>
2022-04-06 15:26:50 +0700

    Add bootstrap and awesome

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,11 +44,17 @@ namespace API
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
-            app.UseHttpsRedirection();
+            }         
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+            );
+
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
