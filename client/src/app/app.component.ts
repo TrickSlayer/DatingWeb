f3b769b382575d97a06bee3305e4aab0635c68e0
TrickSlayer <90195916+TrickSlayer@users.noreply.github.com>
2022-04-13 14:01:17 +0700

    child and parent Component communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,10 +13,9 @@ export class AppComponent implements OnInit{
   users: any;
   subtitle = 'New Line';
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(){
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -26,12 +25,5 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      response => {
-        this.users = response;
-      }, error => {
-        console.log(error);
-      });
-  }
+
 }
